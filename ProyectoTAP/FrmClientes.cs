d6b9edb88c9bbab6cec4a72a714ca8d12c3f586f
ANGELESING <za220111540@zapopan.tecmm.edu.mx>
2024-05-20 13:01:48 -0600

    Creación de usuarios

---
+++ ProyectoTAP/FrmClientes.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 52331
+ * Date: 20/05/2024
+ * Time: 12:33 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProyectoTAP
+{
+	/// <summary>
+	/// Description of FrmClientes.
+	/// </summary>
+	public partial class FrmClientes : Form
+	{
+		public FrmClientes()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
